d158f1b72fdec94140d5e3956cbc7740d1713386
Andre Rocha <andrefmrocha@live.com.pt>
2019-11-29 10:25:21 +0000

    Fixed spaghett

--- proj/LightingScene.js
+++ proj/LightingScene.js
@@ -52,10 +52,10 @@ class LightingScene extends CGFscene{
 	logPicking() {
 		if (this.pickMode == false) {
 			if (this.pickResults != null && this.pickResults.length > 0) {
-				for (var i = 0; i < this.pickResults.length; i++) {
-					var obj = this.pickResults[i][0];
+				for (let i = 0; i < this.pickResults.length; i++) {
+					const obj = this.pickResults[i][0];
 					if (obj) {
-						var customId = this.pickResults[i][1];
+						const customId = this.pickResults[i][1];
 						console.log("Picked object: " + obj + ", with pick id " + customId);						
 					}
 				}
