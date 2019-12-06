05d8604ee15524ad1aba40255d9a76cb4c456fa3
Andre Rocha <andrefmrocha@live.com.pt>
2019-12-06 11:12:00 +0000

    Added endpoint for moves

--- api/server.pl
+++ api/server.pl
@@ -3,24 +3,25 @@
 :-use_module(library(http/json)).
 :- use_module(library(http/json_convert)).
 :-ensure_loaded('board_generation.pl').
-
-
-server:-
-    http_server([port(8080)]).
+:-ensure_loaded('move.pl').
 
 
 :- json_object
     board_size(columns: integer, lines: integer).
+
+
+:-json_object
+    coords(xi: integer, yi: integer, xf: integer, yf: integer).
+
+:- json_object
+    board_move(board: list, move: coords/4, player: integer).
     
 :- json_object
     board(board: list).
 
-:- http_handler(root(.),
-                http_redirect(moved, location_by_id(home_page)),
-                []).
-:- http_handler(root(home), home_page, []).
-
 :-http_handler(root(generate), build_board, []).
+
+:-http_handler(root(move), user_move, []).
 
 build_board(Request):-
     http_read_json(Request, JSONIn),
@@ -31,8 +32,9 @@ build_board(Request):-
     reply_json(JSONOut).
 
 
-home_page(_Request) :-
-    reply_html_page(
-        title('Demo server'),
-        [ h1('Hello world!')
-        ]).
+user_move(Request):-
+    http_read_json(Request, JSONIn),
+    json_to_prolog(JSONIn, board_move(Board, coords(Xi, Yi, Xf, Yf), Player)),
+    move(Board, [Xi, Yi, Xf, Yf], NewBoard, Player),
+    prolog_to_json(board(NewBoard), JSONOut),
+    reply_json(JSONOut).
--- main.pl
+++ main.pl
@@ -1,3 +1,18 @@
+:-use_module(library(http/http_server)).
+:-use_module(library(http/http_server_files)).
+:-use_module(library(http/http_files)).
+:- use_module(library(http/http_path)).
 :- ensure_loaded('api/server.pl').
 
-:-http_server([port(8080)]).
+
+:- multifile user:file_search_path/2.
+
+
+user:file_search_path(document_root,'/srv/htdocs').
+user:file_search_path(game, document_root(proj)).
+
+
+:- http_handler(root(.), http_reply_from_files('.', []), [prefix]).
+
+
+server:-http_server([port(8001)]).
